2612610b630735bdb38f28c31cb53bf5ce474feb
Joseph Chadi Benoit Lemaitre <joseph.lemaitre@epfl.ch>
2014-05-05 11:47:10 +0200

    Awesome plot works + initial state OK

--- hopfieldNetwork.py
+++ hopfieldNetwork.py
@@ -2,29 +2,110 @@ from PIL import Image
 from copy import copy
 from time import sleep
 from pylab import *
-import numpy
+import numpy as np
+
+plot_dic={'cmap':cm.gray,'interpolation':'nearest'}
+
+tmax = 20
 
 class hopfieldNetwork:
     def __init__(self,N):
         self.N = N
+    
     def makePattern(self,P=1,ratio=0.5):
         """
         """
-        self.pattern = -numpy.ones((P,self.N**2))
-        idx = int(ratio*self.N**2)
+        self.pattern = -np.ones((P,self.N))
+        idx = int(ratio*self.N)
         for i in range(P):
             self.pattern[i,:idx] = 1
-            print(self.pattern)
-            self.pattern[i] = numpy.random.permutation(self.pattern[i])
-        print(self.pattern)
-        self.weight = numpy.zeros((self.N,self.N))
+            self.pattern[i] = np.random.permutation(self.pattern[i])
+        
+        self.weight = np.zeros((self.N,self.N))
         for i in range(self.N):
             for j in range(self.N):
                 sum = 0
                 for k in range(P):
                     sum += self.pattern[k,i]*self.pattern[k,j]
                 self.weight[i,j] = 1./self.N * sum
-        print self.weight
+                
+
+    def overlap(self, mu):
+        return (1/self.N)*sum(self.x*self.pattern[mu])
+        
+    def grid(self,mu=None):
+        if mu is not None:
+            x_grid = reshape(self.pattern[mu],(self.N,1))
+        else:
+            x_grid = reshape(self.x,(self.N,1))
+        return x_grid
+    
+    
+    def run(self,mu=0,flip_ratio=0):
+        """
+        """
+        try:
+            self.pattern[mu]
+        except:
+            raise IndexError, 'pattern index too high'
+        
+        # set the initial state of the net
+        self.x = copy(self.pattern[mu])
+        flip = permutation(arange(self.N))
+        idx = int(self.N*flip_ratio)
+        self.x[flip[0:idx]] *= -1
+        
+        t = [0]
+        overlap = [self.overlap(mu)]
+        
+        # prepare the figure
+        figure()
+        
+        # plot the current network state
+        subplot(221)
+        g1 = imshow(self.grid(),**plot_dic)# we keep a handle to the image
+        axis('off')
+        title('x')
+        
+        # plot the target pattern
+        subplot(222)
+        imshow(self.grid(mu=mu),**plot_dic)
+        axis('off')
+        title('pattern %i'%mu)
+        
+        # plot the time course of the overlap
+        subplot(212)
+        g2, = plot(t,overlap,'k',lw=2) # we keep a handle to the curve
+        axis([0,tmax,-1,1])
+        xlabel('time step')
+        ylabel('overlap')
+        
+        # this forces pylab to update and show the fig.
+        draw()
+        #x_old = copy(self.x)
+        
+        #for i in range(tmax):
+
+            ## run a step
+            #self.dynamic()
+            #t.append(i+1)
+            #overlap.append(self.overlap(mu))
+            
+            ## update the plotted data
+            #g1.set_data(self.grid())
+            #g2.set_data(t,overlap)
+            
+            ## update the figure so that we see the changes
+        #draw()
+
+            ## check the exit condition
+            #i_fin = i+1
+            #if sum(abs(x_old-self.x))==0:
+                #break
+            #x_old = copy(self.x)
+            #sleep(0.5)
+        #print 'pattern recovered in %i time steps with final overlap %.3f'%(i_fin,overlap[-1])
 if __name__ == "__main__":
     h = hopfieldNetwork(200)
     h.makePattern()
+    h.run(flip_ratio=0)
